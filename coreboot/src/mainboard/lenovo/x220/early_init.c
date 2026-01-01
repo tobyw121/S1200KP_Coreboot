29a766d4ee282f5440c43ac6ac95f0803223c597
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:46:55 +0100

    Upload Batch 181

---
+++ coreboot/src/mainboard/lenovo/x220/early_init.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <stdint.h>
+#include <northbridge/intel/sandybridge/raminit.h>
+
+void mainboard_fill_pei_data(struct pei_data *pei_data)
+{
+}
