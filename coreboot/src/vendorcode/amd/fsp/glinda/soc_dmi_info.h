2e3ae6c7e41d01d71b6d93ed8fa2578e46e4eaba
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:44:49 +0100

    Upload Batch 94

---
+++ coreboot/src/vendorcode/amd/fsp/glinda/soc_dmi_info.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * These definitions are used to describe memory modules physical layout
+ * TODO: Update for Glinda
+ */
+
+#ifndef SOC_DMI_INFO_H
+#define SOC_DMI_INFO_H
+
+#define AGESA_STRUCT_SOCKET_COUNT         4   ///< Number of sockets in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_CHANNELS_PER_SOCKET  16   ///< Channels per socket in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_DIMMS_PER_CHANNEL    2   ///< DIMMs per channel in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_PART_NUMBER_SIZE     31
+
+#define SMBIOS_3_2_SUPPORT                1
+#define SMBIOS_3_3_SUPPORT                1
+
+#endif /* SOC_DMI_INFO_H */
---
+++ coreboot/src/vendorcode/amd/fsp/phoenix/soc_dmi_info.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * These definitions are used to describe memory modules physical layout
+ */
+
+#ifndef SOC_DMI_INFO_H
+#define SOC_DMI_INFO_H
+
+#define AGESA_STRUCT_SOCKET_COUNT         4   ///< Number of sockets in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_CHANNELS_PER_SOCKET  12  ///< Channels per socket in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_DIMMS_PER_CHANNEL    2   ///< DIMMs per channel in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_PART_NUMBER_SIZE     21
+
+#define SMBIOS_3_2_SUPPORT                1
+#define SMBIOS_3_3_SUPPORT                1
+
+#endif /* SOC_DMI_INFO_H */
