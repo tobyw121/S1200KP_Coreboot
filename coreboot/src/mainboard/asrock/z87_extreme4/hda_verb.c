3a013566fba79666493e99769dad748568a945e4
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:51:31 +0100

    Upload Batch 335

---
+++ coreboot/src/mainboard/asrock/z87_extreme4/hda_verb.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+static const u32 realtek_alc1150_verbs[] = {
+	AZALIA_SUBVENDOR(0, 0x18491151),
+	AZALIA_PIN_CFG(0, 0x11, 0x40000000),
+	AZALIA_PIN_CFG(0, 0x14, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x15, 0x01011012),
+	AZALIA_PIN_CFG(0, 0x16, 0x01016011),
+	AZALIA_PIN_CFG(0, 0x17, AZALIA_PIN_CFG_NC(0)),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19040),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19050),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181304f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214020),
+	AZALIA_PIN_CFG(0, 0x1e, 0x01451130),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Realtek ALC1150",
+		.vendor_id    = 0x10ec0900,
+		.subsystem_id = 0x18491151,
+		.address      = 0,
+		.verbs        = realtek_alc1150_verbs,
+		.verb_count   = ARRAY_SIZE(realtek_alc1150_verbs),
+	},
+	{ /* terminator */ }
+};
+
+AZALIA_ARRAY_SIZES;
