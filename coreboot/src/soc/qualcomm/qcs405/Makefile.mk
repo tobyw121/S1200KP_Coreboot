fe3e64bc32f63b02722927585dbe808bef7b8269
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:44:57 +0100

    Upload Batch 100

---
+++ coreboot/src/soc/qualcomm/qcs405/Makefile.mk
@@ -0,0 +1,32 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+ifeq ($(CONFIG_SOC_QUALCOMM_QCS405),y)
+
+################################################################################
+all-y += clock.c
+all-y += spi.c
+all-y += ../common/timer.c
+all-y += gpio.c
+all-y += i2c.c
+all-y += qup.c
+all-y += blsp.c
+all-$(CONFIG_DRIVERS_UART) += uart.c
+
+################################################################################
+bootblock-y += bootblock.c
+bootblock-y += mmu.c
+
+################################################################################
+romstage-y += cbmem.c
+romstage-y += usb.c
+
+################################################################################
+ramstage-y += soc.c
+ramstage-y += usb.c
+
+################################################################################
+
+CPPFLAGS_common += -Isrc/soc/qualcomm/qcs405/include
+CPPFLAGS_common += -Isrc/soc/qualcomm/common/include
+
+endif
