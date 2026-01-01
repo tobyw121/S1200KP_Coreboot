6933d5982f7ffd94a8e97bcce63a66df828dd2ba
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:44:33 +0100

    Upload Batch 82

---
+++ coreboot/src/vendorcode/amd/pi/00670F00/Include/Filecode.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: CC-PDDC */
+/* Please update the license if adding licensable material. */
+
+/*
+ * Empty file.
+ * coreboot doesn't need the filecodes, but some of the AGESA source need
+ * this file to exist.
+ */
