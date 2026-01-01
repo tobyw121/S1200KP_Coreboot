fbc9af2e5ca64c0314b11cf3d754583cfa12b85e
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:46:58 +0100

    Upload Batch 183

---
+++ coreboot/src/mainboard/lenovo/x200/variants/x200/dock.c
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <console/console.h>
+#include <device/device.h>
+#include <gpio.h>
+#include <ec/lenovo/h8/h8.h>
+#include <ec/acpi/ec.h>
+
+#include "../../dock.h"
+
+void h8_mb_init(void)
+{
+	if (dock_present()) {
+		printk(BIOS_DEBUG, "dock is connected\n");
+		dock_connect();
+	} else {
+		printk(BIOS_DEBUG, "dock is not connected\n");
+	}
+}
+
+void dock_connect(void)
+{
+	ec_set_bit(0x02, 0);
+	gpio_set(28, GPIO_LEVEL_HIGH);
+}
+
+void dock_disconnect(void)
+{
+	ec_clr_bit(0x02, 0);
+	gpio_set(28, GPIO_LEVEL_LOW);
+}
+
+int dock_present(void)
+{
+	const gpio_t dock_id_gpio[] = {2, 3, 4};
+
+	return gpio_base2_value(dock_id_gpio, ARRAY_SIZE(dock_id_gpio)) != 7;
+}
