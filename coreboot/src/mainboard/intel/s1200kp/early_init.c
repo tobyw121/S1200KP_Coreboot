5e10f09bbc76b600797cdd8ffcd8dc3105c58239
Toby <tobiasw763@gmail.com>
2026-01-01 22:18:40 +0100

    Use W83667HG-A super I/O for S1200KP

--- coreboot/src/mainboard/intel/s1200kp/early_init.c
+++ coreboot/src/mainboard/intel/s1200kp/early_init.c
@@ -4,21 +4,20 @@
 #include <device/pci_ops.h>
 #include <device/pnp_ops.h>
 #include <southbridge/intel/bd82x6x/pch.h>
-#include <superio/nuvoton/common/nuvoton.h>
-#include <superio/nuvoton/nct6776/nct6776.h>
+#include <superio/winbond/common/winbond.h>
+#include <superio/winbond/w83667hg-a/w83667hg-a.h>
 
-#define SERIAL_DEV PNP_DEV(0x2e, NCT6776_SP1)
+#define SERIAL_DEV PNP_DEV(0x2e, W83667HG_A_SP1)
 
 void bootblock_mainboard_early_init(void)
 {
-	pci_write_config16(PCI_DEV(0, 0x1f, 0), 0x82, 0x3f0f);
-	pci_write_config16(PCI_DEV(0, 0x1f, 0), 0x80, 0x0010);
+	/*
+	* Mirror the OEM LPC decode ranges so that COM1, the keyboard controller
+	* and the Super I/O configuration ports are available for bootblock and
+	* payload consumers (e.g. SeaBIOS) right from reset.
+	*/
+	pci_write_config16(PCH_LPC_DEV, LPC_EN, 0x3f0f);
+	pci_write_config16(PCH_LPC_DEV, LPC_IO_DEC, 0x0010);
 
-	nuvoton_pnp_enter_conf_state(SERIAL_DEV);
-	pnp_set_logical_device(SERIAL_DEV);
-
-	/* Route SP1 to 0x3f8/IRQ4 for early serial debugging. */
-	nuvoton_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
-
-	nuvoton_pnp_exit_conf_state(SERIAL_DEV);
+	winbond_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }
