cc17368c735137f0e2a490e83671ea5da2a950f1
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:52:05 +0100

    Upload Batch 351

---
+++ coreboot/src/mainboard/getac/p470/early_init.c
@@ -0,0 +1,121 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <arch/io.h>
+#include <bootblock_common.h>
+#include <console/console.h>
+#include <device/pnp_ops.h>
+#include <device/pci_ops.h>
+#include <gpio.h>
+#include <northbridge/intel/i945/i945.h>
+#include <option.h>
+#include <stdint.h>
+#include <southbridge/intel/i82801gx/i82801gx.h>
+
+void mainboard_pre_raminit_config(int s3_resume)
+{
+	printk(BIOS_SPEW, "\n  Initializing drive bay...\n");
+	gpio_set(33, 1); // GPIO33 = ODD
+	gpio_set(34, 1); // GPIO34 = IDE_RST#
+	gpio_set(13, 0); // ??
+
+	printk(BIOS_SPEW, "\n  Initializing Ethernet NIC...\n");
+	gpio_set(24, 0); // Enable LAN Power
+}
+
+/* Override the default lpc decode ranges */
+void mainboard_lpc_decode(void)
+{
+	int lpt_en = 0;
+
+	if (get_uint_option("lpt", 0))
+		lpt_en = LPT_LPC_EN; /* enable LPT */
+
+	// decode range
+	pci_write_config16(PCI_DEV(0, 0x1f, 0), LPC_IO_DEC, 0x0007);
+	// decode range
+	pci_update_config32(PCI_DEV(0, 0x1f, 0), LPC_EN, ~LPT_LPC_EN, lpt_en);
+}
+
+/* This box has two superios, so enabling serial becomes slightly excessive.
+ * We disable a lot of stuff to make sure that there are no conflicts between
+ * the two. Also set up the GPIOs from the beginning. This is the "no schematic
+ * but safe anyways" method.
+ */
+static void pnp_enter_ext_func_mode(pnp_devfn_t dev)
+{
+	unsigned int port = dev >> 8;
+	outb(0x55, port);
+}
+
+static void pnp_exit_ext_func_mode(pnp_devfn_t dev)
+{
+	unsigned int port = dev >> 8;
+	outb(0xaa, port);
+}
+
+void bootblock_mainboard_early_init(void)
+{
+	const pnp_devfn_t dev = PNP_DEV(0x4e, 0x00);
+
+	pnp_enter_ext_func_mode(dev);
+	pnp_write_config(dev, 0x02, 0x0e); // UART power
+	pnp_write_config(dev, 0x1b, (0x3e8 >> 2)); // UART3 base
+	pnp_write_config(dev, 0x1c, (0x2e8 >> 2)); // UART4 base
+	pnp_write_config(dev, 0x1d, (5 << 4) | 11); // UART3,4 IRQ
+	pnp_write_config(dev, 0x1e, 1); // no 32khz clock
+	pnp_write_config(dev, 0x24, (0x3f8 >> 2)); // UART1 base
+	pnp_write_config(dev, 0x28, (4 << 4) | 0); // UART1,2 IRQ
+	pnp_write_config(dev, 0x2c, 0); // DMA0 FIR
+	pnp_write_config(dev, 0x30, (0x600 >> 4)); // Runtime Register Block Base
+
+	pnp_write_config(dev, 0x31, 0xce); // GPIO1 DIR
+	pnp_write_config(dev, 0x32, 0x00); // GPIO1 POL
+	pnp_write_config(dev, 0x33, 0x0f); // GPIO2 DIR
+	pnp_write_config(dev, 0x34, 0x00); // GPIO2 POL
+	pnp_write_config(dev, 0x35, 0xa8); // GPIO3 DIR
+	pnp_write_config(dev, 0x36, 0x00); // GPIO3 POL
+	pnp_write_config(dev, 0x37, 0xa8); // GPIO4 DIR
+	pnp_write_config(dev, 0x38, 0x00); // GPIO4 POL
+
+	pnp_write_config(dev, 0x39, 0x00); // GPIO1 OUT
+	pnp_write_config(dev, 0x40, 0x80); // GPIO2/MISC OUT
+	pnp_write_config(dev, 0x41, 0x00); // GPIO5 OUT
+	pnp_write_config(dev, 0x42, 0xa8); // GPIO5 DIR
+	pnp_write_config(dev, 0x43, 0x00); // GPIO5 POL
+	pnp_write_config(dev, 0x44, 0x00); // GPIO ALT1
+	pnp_write_config(dev, 0x45, 0x50); // GPIO ALT2
+	pnp_write_config(dev, 0x46, 0x00); // GPIO ALT3
+
+	pnp_write_config(dev, 0x48, 0x55); // GPIO ALT5
+	pnp_write_config(dev, 0x49, 0x55); // GPIO ALT6
+	pnp_write_config(dev, 0x4a, 0x55); // GPIO ALT7
+	pnp_write_config(dev, 0x4b, 0x55); // GPIO ALT8
+	pnp_write_config(dev, 0x4c, 0x55); // GPIO ALT9
+	pnp_write_config(dev, 0x4d, 0x55); // GPIO ALT10
+
+	pnp_exit_ext_func_mode(dev);
+}
+
+void mainboard_late_rcba_config(void)
+{
+	/* Device 1f interrupt pin register */
+	RCBA32(D31IP) = 0x00042220;
+	/* Device 1d interrupt pin register */
+	RCBA32(D28IP) = 0x00214321;
+
+	/* dev irq route register */
+	RCBA16(D31IR) = 0x0232;
+	RCBA16(D30IR) = 0x3246;
+	RCBA16(D29IR) = 0x0237;
+	RCBA16(D28IR) = 0x3201;
+	RCBA16(D27IR) = 0x3216;
+
+	/* Disable unused devices */
+	RCBA32(FD) |= FD_INTLAN;
+
+	/* This should probably go into the ACPI enable trap */
+
+	/* Set up I/O Trap #3 for 0x800-0x80c (Trap) */
+	RCBA32(0x1e9c) = 0x000200f0;
+	RCBA32(0x1e98) = 0x000c0801;
+}
---
+++ coreboot/src/mainboard/getac/p470/ec_oem.c
@@ -0,0 +1,98 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <console/console.h>
+#include <arch/io.h>
+#include <delay.h>
+#include <ec/acpi/ec.h>
+#include <types.h>
+
+#include "ec_oem.h"
+
+enum cb_err send_ec_oem_command(u8 command)
+{
+	int timeout;
+
+	timeout = 0x7ff;
+	while ((inb(EC_OEM_SC) & EC_IBF) && --timeout) {
+		udelay(10);
+		if ((timeout & 0xff) == 0)
+			printk(BIOS_SPEW, ".");
+	}
+	if (!timeout) {
+		printk(BIOS_DEBUG, "Timeout while sending OEM command 0x%02x to EC!\n",
+				command);
+		// return CB_ERR;
+	}
+
+	outb(command, EC_OEM_SC);
+	return CB_SUCCESS;
+}
+
+enum cb_err send_ec_oem_data(u8 data)
+{
+	int timeout;
+
+	timeout = 0x7ff;
+	while ((inb(EC_OEM_SC) & EC_IBF) && --timeout) { // wait for IBF = 0
+		udelay(10);
+		if ((timeout & 0xff) == 0)
+			printk(BIOS_SPEW, ".");
+	}
+	if (!timeout) {
+		printk(BIOS_DEBUG, "Timeout while sending OEM data 0x%02x to EC!\n",
+				data);
+		// return CB_ERR;
+	}
+
+	outb(data, EC_OEM_DATA);
+
+	return CB_SUCCESS;
+}
+
+u8 recv_ec_oem_data(void)
+{
+	int timeout;
+	u8 data;
+
+	timeout = 0x7fff;
+	while (--timeout) { // Wait for OBF = 1
+		if (inb(EC_OEM_SC) & EC_OBF) {
+			break;
+		}
+		udelay(10);
+		if ((timeout & 0xff) == 0)
+			printk(BIOS_SPEW, ".");
+	}
+	if (!timeout) {
+		printk(BIOS_DEBUG, "\nTimeout while receiving OEM data from EC!\n");
+		// return -1;
+	}
+
+	data = inb(EC_OEM_DATA);
+	// printk(BIOS_SPEW, "recv_ec_oem_data: 0x%02x\n", data);
+
+	return data;
+}
+
+u8 ec_oem_read(u8 addr)
+{
+	send_ec_oem_command(0x80);
+	send_ec_oem_data(addr);
+
+	return recv_ec_oem_data();
+}
+
+int ec_oem_dump_status(void)
+{
+	u8 ec_sc = inb(EC_OEM_SC);
+	printk(BIOS_DEBUG, "Embedded Controller Status: ");
+	if (ec_sc & (1 << 6)) printk(BIOS_DEBUG, "SMI_EVT ");
+	if (ec_sc & (1 << 5)) printk(BIOS_DEBUG, "SCI_EVT ");
+	if (ec_sc & (1 << 4)) printk(BIOS_DEBUG, "BURST ");
+	if (ec_sc & (1 << 3)) printk(BIOS_DEBUG, "CMD ");
+	if (ec_sc & (1 << 1)) printk(BIOS_DEBUG, "IBF ");
+	if (ec_sc & (1 << 0)) printk(BIOS_DEBUG, "OBF ");
+	printk(BIOS_DEBUG, "\n");
+
+	return ec_sc;
+}
