66c3b6c26abb3d2f08f7bbb155d975e29094d6cf
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:53:13 +0100

    Upload Batch 382

---
+++ coreboot/src/superio/ite/Makefile.mk
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+## include generic ite pre-ram stage driver
+bootblock-$(CONFIG_SUPERIO_ITE_COMMON_PRE_RAM) += common/early_serial.c
+romstage-$(CONFIG_SUPERIO_ITE_COMMON_PRE_RAM) += common/early_serial.c
+
+bootblock-$(CONFIG_SUPERIO_ITE_COMMON_GPIO_PRE_RAM) += common/gpio.c
+romstage-$(CONFIG_SUPERIO_ITE_COMMON_GPIO_PRE_RAM) += common/gpio.c
+
+## include generic ite environment controller driver
+ramstage-$(CONFIG_SUPERIO_ITE_ENV_CTRL) += common/env_ctrl.c
+
+## include generic ite driver to smm to control S3-relevant functions
+smm-$(CONFIG_SUPERIO_ITE_COMMON_PRE_RAM) += common/early_serial.c
+smm-$(CONFIG_SUPERIO_ITE_COMMON_GPIO_PRE_RAM) += common/gpio.c
+
+subdirs-y += it8528e
+subdirs-y += it8613e
+subdirs-y += it8623e
+subdirs-y += it8629e
+subdirs-y += it8659e
+subdirs-y += it8712f
+subdirs-y += it8718f
+subdirs-y += it8720f
+subdirs-y += it8721f
+subdirs-y += it8728f
+subdirs-y += it8772f
+subdirs-y += it8783ef
+subdirs-y += it8784e
+subdirs-y += it8786e
---
+++ coreboot/src/superio/ite/it8659e/it8659e.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef SUPERIO_ITE_IT8659E_H
+#define SUPERIO_ITE_IT8659E_H
+
+#define IT8659E_SP1  0x01 /* Com1 */
+#define IT8659E_SP2  0x02 /* Com2 */
+#define IT8659E_EC   0x04 /* Environment controller */
+#define IT8659E_KBCK 0x05 /* PS/2 keyboard */
+#define IT8659E_KBCM 0x06 /* PS/2 mouse */
+#define IT8659E_GPIO 0x07 /* GPIO */
+#define IT8659E_CIR  0x0A /* CIR */
+
+#endif /* SUPERIO_ITE_IT8659E_H */
