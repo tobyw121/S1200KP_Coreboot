108588afc347451a9907a0994850b1a02b6abf73
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:51:05 +0100

    Upload Batch 322

---
+++ coreboot/src/mainboard/intel/emeraldlake2/early_init.c
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <bootblock_common.h>
+#include <stdint.h>
+#include <arch/io.h>
+#include <superio/smsc/sio1007/sio1007.h>
+#include <northbridge/intel/sandybridge/raminit.h>
+
+#define SIO_PORT 0x164e
+
+void bootblock_mainboard_early_init(void)
+{
+	const u16 port = SIO_PORT;
+	const u16 runtime_port = 0x180;
+
+	sio1007_enable_uart_at(port);
+
+	/* Turn on configuration mode. */
+	outb(0x55, port);
+
+	/* Set the GPIO direction, polarity, and type. */
+	sio1007_setreg(port, 0x31, 1 << 0, 1 << 0);
+	sio1007_setreg(port, 0x32, 0 << 0, 1 << 0);
+	sio1007_setreg(port, 0x33, 0 << 0, 1 << 0);
+
+	/* Set the base address for the runtime register block. */
+	sio1007_setreg(port, 0x30, runtime_port >> 4, 0xff);
+	sio1007_setreg(port, 0x21, runtime_port >> 12, 0xff);
+
+	/* Turn on address decoding for it. */
+	sio1007_setreg(port, 0x3a, 1 << 1, 1 << 1);
+
+	/* Set the value of GPIO 10 by changing GP1, bit 0. */
+	u8 byte;
+	byte = inb(runtime_port + 0xc);
+	byte |= (1 << 0);
+	outb(byte, runtime_port + 0xc);
+
+	/* Turn off address decoding for it. */
+	sio1007_setreg(port, 0x3a, 0 << 1, 1 << 1);
+
+	/* Turn off configuration mode. */
+	outb(0xaa, port);
+}
+
+void mainboard_fill_pei_data(struct pei_data *pei_data)
+{
+}
