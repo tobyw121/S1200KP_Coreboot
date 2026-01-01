677698fa22cd0944cd25b64d8b635c3d8aaa87f1
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 03:48:53 +0100

    Upload Batch 253

---
+++ coreboot/src/mainboard/google/glados/spd/spd.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef MAINBOARD_SPD_H
+#define MAINBOARD_SPD_H
+
+#define SPD_DENSITY_BANKS	4
+#define SPD_ADDRESSING		5
+#define SPD_ORGANIZATION	7
+#define SPD_BUS_DEV_WIDTH	8
+
+#endif
