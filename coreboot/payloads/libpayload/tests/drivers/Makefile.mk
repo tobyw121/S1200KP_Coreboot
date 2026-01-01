822afb5c6157ab46aa78004d5a93d9b31fbea94e
tobyw121 <Tobiasw763@gmail.com>
2026-01-01 04:06:22 +0100

    Upload Batch 628

---
+++ coreboot/payloads/libpayload/tests/drivers/Makefile.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+tests-y += graphics-test speaker-test
+
+graphics-test-srcs += tests/drivers/graphics-test.c
+graphics-test-srcs += libc/fpmath.c
+
+speaker-test-srcs += tests/drivers/speaker-test.c
+speaker-test-mocks += inb
+speaker-test-mocks += outb
